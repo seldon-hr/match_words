755b16f5e5c8b684cc3aa4b5bcd7408daefbc49f
seldon-hr1 <daanmaro99june@gmail.com>
2024-09-27 14:18:45 -0600

    fix: "Use of text interpretos"

--- exercises/01_py.py
+++ exercises/01_py.py
@@ -1,1 +1,7 @@
-print("Twinkle, twinkle, little star,\n\t How I wonder waht you are!\n\t\t Up above the world so hight,\n Like a dimond in the sky. \nTwinkle, twinkle, little star, \n\t How I wonder waht you are!")
+# print("Twinkle, twinkle, little star,\n\t How I wonder waht you are!\n\t\t Up above the world so hight,\n Like a dimond in the sky. \nTwinkle, twinkle, little star, \n\t How I wonder waht you are!")
+""" Use in text for python
+\n = new line
+\t = tab
+\r = carriage return
+ """
+print("Dan, dan, are you ready, ready? \r Are you ready?, Let's go...")
